183ed112aa767a29ac11e7264125f01c0bda393a
Isabelle Ferreira <isabelle.ferreira3000@gmail.com>
2019-03-23 17:07:31 -0300

    implementing database

--- main.cs
+++ main.cs
@@ -1,16 +1,16 @@
 using System;
 using System.Collections.Generic;
 
-public class Database
+public static class Database
 {
-	public List<Seller> sellers = new List<Seller>();
-	public List<Customer> customers = new List<Customer>();
-	public List<Review> reviews = new List<Review>();
+	public static List<Seller> sellers = new List<Seller>();
+	public static List<Customer> customers = new List<Customer>();
+	public static List<Review> reviews = new List<Review>();
 	
-	public Seller getSellerById(int id_seller) {
+	public static Seller getSellerById(int id_seller) {
 		Seller sellerFinded = new Seller();
 		
-		foreach(Seller seller in this.sellers) {
+		foreach(Seller seller in sellers) {
             if(seller.id == id_seller){
 				sellerFinded = seller;
 				break;
@@ -20,10 +20,10 @@ public class Database
 		return sellerFinded;
 	}
 	
-	public Customer getCustomerById(int id_customer) {
+	public static Customer getCustomerById(int id_customer) {
 		Customer customerFinded = new Customer();
 		
-		foreach(Customer customer in this.customers) {
+		foreach(Customer customer in customers) {
             if(customer.id == id_customer){
 				customerFinded = customer;
 				break;
@@ -36,10 +36,22 @@ public class Database
 
 public class Review
 {
+	public static int count = 0;
+	
+	public int id;
 	public int id_seller;
 	public int id_customer;
 	public int note;
 	public string text;
+	
+	public Review(int id_seller, int id_customer, int note, string text){
+		count = count + 1;
+		this.id = count;
+		this.id_seller = id_seller;
+		this.id_customer = id_customer;
+		this.note = note;
+		this.text = text;
+	}
 }
 
 public class Goods
@@ -65,9 +77,9 @@ public class Customer
 	public string name;
 	// public List<Review> reviews = new List<Review>();
 	
-	public void addReview(int id_seller, string text, int note){
-		
-		
+	public void addReview(int id_seller, int note, string text){
+		Review newReview = new Review(id_seller, this.id, note, text);
+		Database.reviews.Add(newReview);
 	}
 }
 
